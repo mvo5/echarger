ecdf69346ccac5f92d9397a271509fae23cba248
Michael Vogt <mvo@ubuntu.com>
2022-02-13 15:28:37 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+# Charge eletric car optimally via solar
+
+The script reads the current solar power and adjusts the
+charger for the eletric car accordingly.
+
+Currently only go-e and solarmax SMT are supported.
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+    name="eloader-mvo",
+    version="0.1",
+    description="optimal e-car charging",
+    author="Michael Vogt",
+    author_email="michael.vogt@gmail.com",
+    url="https://github.com/mvo5/echarger",
+    scripts=["eloader"],
+)
