4d913716acbbd62677591cbda201a0c0f85070d0
Michael Vogt <mvo@ubuntu.com>
2022-05-07 17:30:34 +0200

    eloader: fix crash in date compare (this really needs a refactor and tests :/

--- tests/test_sunrise.py
+++ tests/test_sunrise.py
@@ -12,11 +12,11 @@ class TestSunsetDawn(unittest.TestCase):
         # just basic sanity test for Germany
         self.assertTrue(sunrise < sunset)
         # sunrise
-        self.assertTrue(datetime.time(4, 30, 0) < sunrise.time())
-        self.assertTrue(datetime.time(7, 31, 0) > sunrise.time())
+        self.assertTrue(datetime.time(4, 30, 0) < sunrise)
+        self.assertTrue(datetime.time(7, 31, 0) > sunrise)
         # sunset
-        self.assertTrue(datetime.time(17, 30, 0) < sunset.time())
-        self.assertTrue(datetime.time(22, 0, 0) > sunset.time())
+        self.assertTrue(datetime.time(17, 30, 0) < sunset)
+        self.assertTrue(datetime.time(22, 0, 0) > sunset)
 
     def test_sunrise_sunset_precise(self):
         sunrise, sunset = get_sunrise_sunset("Berlin", date=datetime.date(2022, 5, 7))
