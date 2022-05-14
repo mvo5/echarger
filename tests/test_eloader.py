b45577a7c3a744962727197d4afd4bacc7ae9fa5
Michael Vogt <mvo@ubuntu.com>
2022-05-14 22:20:36 +0200

    eloader: extract EloaderController.manual_overriden() helper and add tests

--- tests/test_eloader.py
+++ tests/test_eloader.py
@@ -27,3 +27,21 @@ class TestEloader(unittest.TestCase):
         self.assertEqual(self.el.tick(), 360)
         self.assertTrue(self.el.goe.force_pause)
         self.el.log.assert_called_with("not enough power, forcing pause")
+
+    def test_eloader_manual_overriden(self):
+        test_cases = [
+            # paused or no car, divergence does not matter
+            {"pau": True, "con": True, "cur_amp": 10, "goe_amp": 16, "exp": False},
+            {"pau": False, "con": False, "cur_amp": 10, "goe_amp": 16, "exp": False},
+            {"pau": True, "con": False, "cur_amp": 10, "goe_amp": 16, "exp": False},
+            # manual overriden by user
+            {"pau": False, "con": True, "cur_amp": 10, "goe_amp": 16, "exp": True},
+        ]
+        for t in test_cases:
+            self.el.goe.force_pause = t["pau"]
+            self.el.goe.car_connected = t["con"]
+            self.el.current_ampere = t["cur_amp"]
+            self.el.goe.ampere = t["goe_amp"]
+            self.el.current_phases = 1
+            self.el.goe.phases = 1
+            self.assertEqual(self.el.manual_overriden(), t["exp"], msg=t)
